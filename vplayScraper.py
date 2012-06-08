ff36ed9deb2e9ce8b0bf19f07b1391979f83677f
Cornel Damian <cornel.damian@ymail.com>
2012-06-09 00:37:58 +0300

    separate serials from favorite

--- vplayScraper.py
+++ vplayScraper.py
@@ -3,7 +3,12 @@ import re
 class Scrap:
 
     def scrapFavorite(self, page):
-        match=re.compile('<a href="(/serials/browse.do\?sid=[0-9]+)" target="_top" class="group-item"(.+?)title="(.+?)"><img src="(.+?)" width="312" height="103">(.+?)</a>').findall(page)
+        pos = page.find("<h2>Colec");
+        if pos == -1:
+            return [];
+        page = page[:pos]
+        print page
+        match=re.compile('<a href="(/c/.+?/)" title="(.+?)"><span class="coll_poster" title="(.+?)" style="background-image:url\((.+?)\);"></span>').findall(page);
         return match
 
     def scrapSearch(self, page):
@@ -11,7 +16,12 @@ class Scrap:
         return match
         
     def scrapSerials(self, page):
-        match=re.compile('<a href="(/c/.+?/)" title="(.+?)"><span class="coll_poster" title="(.+?)" style="background-image:url\((.+?)\);"></span>').findall(page)
+        print "Scrap Seriale"
+        pos = page.find("<h2>Colec");
+        if pos == -1:
+            return [];
+        page = page[pos:]
+        match=re.compile('<a href="(/c/.+?/)" title="(.+?)"><span class="coll_poster" title="(.+?)" style="background-image:url\((.+?)\);"></span>').findall(page);
         return match
         
     def scrapSeasons(self, page):
